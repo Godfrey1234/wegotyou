5c9f2602df11bc597d734b04b99f6ff28bfa3159
DA-Mac110 <da-mac110@DA-Mac110s-MacBook-Air.local>
2022-11-24 23:40:31 -0800

    updates

---
+++ backend/app/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+
+
+
+app.use(bodyParser.json());
+
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,DELETE,PUT,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();    
+});
+
+app.get(``, (req, res) => { 
+
+    res.json({info:'Node.js,Express, and Postgres API'})
+ });
+
+
+
+// Listen to the specified port, otherwise 3000
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log(`Server Running: http://localhost:${PORT}`);
+});
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
